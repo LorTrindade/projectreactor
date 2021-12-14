b4eb0217e9d2cf8f383fcc4e1710019528e49ba2
lortrindade <lortrindade@gmail.com>
2021-12-13 21:10:53 -0300

    Sec 07 - classes from monday, 13 Dec

--- src/main/java/com/projectreactor/projectreactor/sec07/Lec03Latest.java
+++ src/main/java/com/projectreactor/projectreactor/sec07/Lec03Latest.java
@@ -3,32 +3,29 @@ package com.projectreactor.projectreactor.sec07;
 import com.projectreactor.projectreactor.courseutil.Util;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
-import reactor.util.concurrent.Queues;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Lec02Drop {
+public class Lec03Latest {
 
     public static void main(String[] args) {
 
         System.setProperty("reactor.bufferSize.small", "16");
 
-        List<Object> list = new ArrayList<>();
-
         Flux.create(fluxSink -> {
-            for (int i = 1; i < 201; i++) {
-                fluxSink.next(i);
-                System.out.println("Pushed: " + i);
-                Util.sleepMillis(1);
-            }
-            fluxSink.complete();
-        })
-                .onBackpressureDrop(list::add)
+                    for (int i = 1; i < 201; i++) {
+                        fluxSink.next(i);
+                        System.out.println("Pushed: " + i);
+                        Util.sleepMillis(1);
+                    }
+                    fluxSink.complete();
+                })
+                .onBackpressureLatest()
                 .publishOn(Schedulers.boundedElastic())
                 .doOnNext(i -> Util.sleepMillis(10))
                 .subscribe(Util.subscriber());
 
         Util.sleepSeconds(10);
-        System.out.println(list);    }
+    }
 }
